205bd1281321abaf80f655a8e01ec05bfba3ed0f
kero_hirake <k16.h.white@gmail.com>
2022-01-11 15:46:10 +0900

    lein new app

---
+++ test/the_money_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns the-money-example.core-test
+  (:require [clojure.test :refer :all]
+            [the-money-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
