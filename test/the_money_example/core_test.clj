04608177076e0469ff0c922956afc94020496453
kero_hirake <k16.h.white@gmail.com>
2022-01-12 11:26:18 +0900

    金額計算のテストのみ

--- test/the_money_example/core_test.clj
+++ test/the_money_example/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [the-money-example.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest money-test
+  (testing "multiplicatoin"
+    (let [five (->Doller 5)]
+      (t/is (= 10 (times five 2))))))
