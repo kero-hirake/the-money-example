5577baba793bc078b0b4ace246f9158463caec87
kero_hirake <k16.h.white@gmail.com>
2022-01-13 11:58:38 +0900

    eq をまとめる ?

--- src/the_money_example/core.clj
+++ src/the_money_example/core.clj
@@ -2,24 +2,20 @@
   (:gen-class))
 
 (defprotocol Calc
-  (times [this multiplier])
-  (eq [this object]))
+  (times [this multiplier]))
+
+(defn eq [obj1 obj2]
+  ;{:pre [(instance? Calc obj1)]}
+  (and
+   (= (:amount obj1) (:amount obj2))
+   (= (class obj1) (class obj2))))
 
 (defrecord Dollar [amount]
   Calc
   (times [this multiplier]
-    (->Dollar (* amount multiplier)))
-  (eq [this object]
-    (and
-     (= amount (:amount object))
-     (= (class this) (class object)))))
+    (->Dollar (* amount multiplier))))
 
 (defrecord Franc [amount]
   Calc
   (times [this multiplier]
-    (->Franc (* amount multiplier)))
-  (eq [this object]
-      (and
-       (= amount (:amount object))
-       (= (class this) (class object))))
-  )
+    (->Franc (* amount multiplier))))
