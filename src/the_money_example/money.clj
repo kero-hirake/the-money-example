6537255ff39523eae5cfe9815947084dfefc6e69
kero_hirake <k16.h.white@gmail.com>
2022-01-24 15:54:32 +0900

    plus は Sum を返す

--- src/the_money_example/money.clj
+++ src/the_money_example/money.clj
@@ -1,4 +1,6 @@
-(ns the-money-example.money)
+(ns the-money-example.money
+  (:require [the-money-example.sum :as sum]
+            [the-money-example.expression :as expression]))
 
 (defprotocol IMoney
   (eq [this other])
@@ -14,7 +16,8 @@
   (times [this mulitplier]
     (->Money (* amount mulitplier) currency))
   (plus [this addend]
-    (->Money (+ amount (:amount addend)) currency)))
+    (sum/->Sum this addend))
+  expression/IExpression)
 
 (defn dollar [amount]
   (->Money amount :USD))
--- test/the_money_example/core_test.clj
+++ test/the_money_example/core_test.clj
@@ -4,33 +4,37 @@
             [the-money-example.bank :as bank]))
 
 (deftest money-test
-  (testing "掛け算"
+  (testing "掛け算 (multiplication)"
     (testing "5 USD * 2 = 10 USD"
       (let [five (money/dollar 5)]
         (is (= (money/dollar 10) (money/times five 2)))))
     (testing "5 USD * 3 = 15 USD"
       (let [five (money/dollar 5)]
         (is (= (money/dollar 15) (money/times five 3))))))
-  (testing "等価性比較"
+  (testing "等価性比較 (Equality)"
     (testing "5 USD = 5 USD"
       (is (money/eq (money/dollar 5) (money/dollar 5))))
     (testing "5 USD != 6 USD"
       (is (not (money/eq (money/dollar 5) (money/dollar 6)))))
     (testing "5 CHF != 5 USD"
       (is (not (money/eq (money/franc 5) (money/dollar 5))))))
-  (testing "通貨"
+  (testing "通貨 (Currency)"
     (testing "1 USD の通貨は :USD"
       (is (= :USD (:currency (money/dollar 1)))))
     (testing "1 CHF の通貨は :CHF"
       (is (= :CHF (:currency (money/franc 1))))))
   (testing "足し算"
-    (testing "5 USD + 5 USD = 10 USD"
+    (testing "5 USD + 5 USD = 10 USD (simple addition)"
       (let [usd5 (money/dollar 5)
             sum (money/plus usd5 usd5)
             bank (bank/->Bank)
             redu (bank/redu bank sum :USD)]
-        (is (= (money/dollar 10) redu)))
-      (let [sum (money/plus (money/dollar 5) (money/dollar 5))]
-        (is (= (money/dollar 10) sum))))
+        (is (= (money/dollar 10) redu))))
+    (testing "plus は sum を返す(plus return sum)"
+      (let [usd5 (money/dollar 5)
+            exp (money/plus usd5 usd5) ;return Expression
+            sum exp]  ; Expression to Sum
+        (is (= usd5 (:augend sum)))
+        (is (= usd5 (:addend sum)))))
     )
-  )
+
