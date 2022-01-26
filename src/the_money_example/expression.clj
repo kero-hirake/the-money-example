dc474d64488487efee62dae2901c63d1fda0bea7
kero_hirake <k16.h.white@gmail.com>
2022-01-26 12:03:15 +0900

    reduce money を通す

--- src/the_money_example/expression.clj
+++ src/the_money_example/expression.clj
@@ -1,6 +1,9 @@
 (ns the-money-example.expression
-  (:require [the-money-example.money :as money]))
+  (:require [the-money-example.money :as money])
+  (:import [the_money_example.money Money]))
 
 (defn redu [source to]
-      (let [amount (+ (-> source  :augend :amount) (-> source  :addend :amount))]
-        (money/->Money amount to)))
+  (if (= (class source) Money)
+    source
+    (let [amount (+ (-> source  :augend :amount) (-> source  :addend :amount))]
+      (money/->Money amount to))))
